a4d0b7e4397af0662330c14b1b3091cf9b83cb40
NeginMiri <Negimiri09@gmail.com>
2026-02-19 18:06:21 +0330

    v1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+const bell_btnEl = document.getElementById("bell-btn");
+const listEl = document.getElementById("list")
+const markEl = document.getElementById("mark")
+const notif_newEl = document.getElementById("notif-new");
+const notif_youEl = document.getElementById("notif-you");
+const notif_orderEl = document.getElementById("notif-order");
+
+bell_btnEl.addEventListener("click", () =>{
+    listEl.classList.toggle("!flex");
+})
+
+
